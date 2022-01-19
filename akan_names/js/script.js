2d39cd92a67f7f545a287c65954d2029f8dc5328
njenganelson <nkamau2@gmail.com>
2022-01-19 10:31:40 +0300

    declare variables in an array

--- akan_names/js/script.js
+++ akan_names/js/script.js
@@ -1,1 +1,13 @@
-alert("Ready for JS")
+//alert("Ready for JS")
+var CC, YY, MM, DD, d, dayValue;
+var dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", " Yaa", "Afua", "Ama"];
+
+function validate(){
+
+}
+function getDay(){
+
+}
+
