92c6ca52eace0889d8a3eb9498e8040f8160f8bd
njenganelson <nkamau2@gmail.com>
2022-01-19 10:44:56 +0300

    test formula for different days

--- akan_names/js/script.js
+++ akan_names/js/script.js
@@ -4,10 +4,18 @@ var dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"
 var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", " Yaa", "Afua", "Ama"];
 
-function validate(){
+function validate() {
 
 }
-function getDay(){
-
+function getDay() {
+    CC = 20
+    YY = 21
+    MM = 12
+    DD = 25
+    d = (((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (MM + 1) / 10)) + DD) % 7
+    console.log(d)
+    dayOfWeek = Math.floor(d)
+    console.log(dayOfWeek)
 }
+getDay()
 
