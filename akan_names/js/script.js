a9974fd222e53605969cb9685d939d08a678970f
njenganelson <nkamau2@gmail.com>
2022-01-19 10:59:04 +0300

    get value from html

--- akan_names/js/script.js
+++ akan_names/js/script.js
@@ -8,14 +8,17 @@ function validate() {
 
 }
 function getDay() {
-    CC = 20
+    /*CC = 20
     YY = 21
     MM = 12
-    DD = 25
+    DD = 25*/
+    year = document.getElementById("year").value
+    CC = parseInt(year.substring(0, 2))
+    YY = parseInt(year.substring(2, 4))
+    MM = parseInt(document.getElementById("month").value)
+    DD = parseInt(document.getElementById("date").value)
     d = (((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (MM + 1) / 10)) + DD) % 7
     console.log(d)
     dayOfWeek = Math.floor(d)
     console.log(dayOfWeek)
 }
-getDay()
-
